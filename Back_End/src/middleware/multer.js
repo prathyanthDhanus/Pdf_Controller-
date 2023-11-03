40a7f364304a9d6e59723003bb1f37af2ecdb28f
prathyanthDhanus <prathyanthbusinessmail@gmail.com>
2023-11-03 10:12:43 +0530

    added env

--- Back_End/src/middleware/multer.js
+++ Back_End/src/middleware/multer.js
@@ -1,19 +1,4 @@
-// const multer = require("multer")
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, './files')
-//     },
-//     filename: function (req, file, cb) {
-//       const uniqueSuffix = Date.now() 
-
-//       cb(null,uniqueSuffix+file.originalname)
-//     }
-//   })
-  
-//   const upload = multer({ storage: storage })
-
-//   module.exports = upload
 const multer = require('multer')
 
 const upload = multer({
